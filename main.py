6aa8134701d29b804c5a0cedebed405f5cc6a6e0
Lidiia Trotchenko <li.trot.it@gmail.com>
2022-12-08 12:43:36 +0100

    Make some change in canvachanges in main.py. Rectangle doen't go inside.

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 from tkinter import *
 
-class Box(object):
-    def __init__(self):
-        self.testBoxX = 300
-        self.testBoxY = 300
 
-    def draw(self, canvas):
-        canvas.create_rectangle(0, 0, 600, 600, fill='white')
-        canvas.create_rectangle(self.testBoxX, self.testBoxY, self.testBoxX+100, self.testBoxY+100, fill='lime green')
+class Box(object):
+    """Class for drawing a rectangle inside canvas."""
+
+    def __init__(self):
+        """Initialise Box with coordinate parameters."""
+        self.test_box_x = 0
+        self.test_box_y = 0
+
+    def draw(self, canvas_board):
+        """Draw rectangle on new position."""
+        canvas_board.create_rectangle(0, 0, 600, 600, fill='white')
+        canvas_board.create_rectangle(self.test_box_x, self.test_box_y,
+                                      self.test_box_x+100, self.test_box_y+100, fill='lime green')
+
 
 # Create the tk environment as usual
+WIDTH = 600
+HEIGHT = 600
 root = Tk()
-canvas = Canvas(root, width=600, height=600)
+canvas = Canvas(root, width=WIDTH, height=HEIGHT)
 
 # Creating a box that can draw itself in a certain position
 box = Box()
 
 # Create a function that can be called when a key pressing happens
-def on_key_press(e):
-    # When the keycode is 111 (up arrow) we move the position of our box higher
-    if e.keycode == 111:
-        box.testBoxY = box.testBoxY - 100
-    elif e.keycode == 116:
-        box.testBoxY = box.testBoxY + 100
+
+
+def on_key_press(enter):
+    """When arrows are pressed the box moves i appropriate way on 100."""
+    if enter.keycode == 38:
+        if box.test_box_y - 100 >= 0:
+            box.test_box_y = box.test_box_y - 100
+    elif enter.keycode == 40:
+        if box.test_box_y + 100 <= HEIGHT - 100:
+            box.test_box_y = box.test_box_y + 100
+    elif enter.keycode == 39:
+        if box.test_box_x + 100 <= WIDTH - 100:
+            box.test_box_x = box.test_box_x + 100
+    elif enter.keycode == 37:
+        if box.test_box_x - 100 >= 0:
+            box.test_box_x = box.test_box_x - 100
     # and lower if the key that was pressed the down arrow
     # draw the box again in the new position
     box.draw(canvas)
+
 
 # Tell the canvas that we prepared a function that can deal with the key press events
 canvas.bind("<KeyPress>", on_key_press)
